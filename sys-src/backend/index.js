877f3274317b911366ffb0854736f2337281f8df
Aaron Kreuzer <a.kreuzer@oth-aw.de>
2022-05-19 14:08:24 +0200

    base docker structure WIP
    Former-commit-id: 3d67e68f30959ad0f8d42a1169d5fa5bafa744d9

---
+++ sys-src/backend/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const socketIo = require('socket.io')
+const http = require('http')
+const PORT = process.env.PORT || 3001
+const app = express()
+const server = http.createServer(app)
+const io = socketIo(server,{ 
+    cors: {
+      origin: 'http://localhost:3000'
+    }
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+io.on('connection',(socket)=>{
+  console.log('client connected: ',socket.id)
+  
+  socket.join('clock-room')
+  
+  socket.on('disconnect',(reason)=>{
+    console.log(reason)
+  })
+})
+
+setInterval(()=>{
+     io.to('clock-room').emit('time', new Date())
+},1000)
+
+server.listen(PORT, err=> {
+  if(err) console.log(err)
+  console.log('Server running on Port ', PORT)
+})
