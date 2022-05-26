2a02794dbda2a1ef90c8f14305a62454d06eef2c
Aaron Kreuzer <a.kreuzer@oth-aw.de>
2022-05-26 13:37:45 +0200

    backend structure, dockercompose changes for mongodb
    Former-commit-id: 0659d40d2a585c0e5e2f6e9fbed6ff2c11b10882

--- sys-src/backend/index.js
+++ sys-src/backend/index.js
@@ -10,20 +10,50 @@ const io = socketIo(server,{
     }
 })
 
+//Set up mongoose connection
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb://mongo:27017/scradles';
+
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+
+const db = mongoose.connection;
+
+db.on('connected', console.log.bind(console, 'Connected to mongoDB'));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+//Router
+let leaderboard = require('./routes/leaderboardRoute')
+
+//Middleware
+
+//App
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.use('/leaderboard', leaderboard)
+
+//Socket
 io.on('connection',(socket)=>{
   console.log('client connected: ',socket.id)
   
+  //testing
   socket.join('clock-room')
   
   socket.on('disconnect',(reason)=>{
     console.log(reason)
   })
+
+  socket.on("beispiel1",()=>{
+    //DOTHIS
+  })
+
+  socket.on("beispiel2",()=>{
+    //DOTHAT
+  })
 })
 
+//testing
 setInterval(()=>{
      io.to('clock-room').emit('time', new Date())
 },1000)
