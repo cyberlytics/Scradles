39e2f179fa3d4ff62d7ec6c2229f8749d99ae4ef
Leonard Woellmer <l.woellmer@oth-aw.de>
2022-05-30 14:12:29 +0200

    Split Pregame and Ingame screens

--- sys-src/frontend/src/components/App/App.js
+++ sys-src/frontend/src/components/App/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 import React from 'react'
 import {io} from 'socket.io-client'
-import GameBoard from '../GameBoard/GameBoard';
-import ScoreBoard from '../ScoreBoard/ScoreBoard';
+import Ingame from '../Ingame/Ingame';
+import Pregame from '../Pregame/Pregame';
 
 const App= ()=> {
     const [time, setTime] = React.useState('fetching')  
@@ -16,27 +16,15 @@ const App= ()=> {
         socket.on('time', (data)=>setTime(data))
         socket.on('disconnect',()=>setTime('server disconnected'))
  
-  },[]) 
-  return (
-      <div className="App">
+    },[]) 
+    return (
+        <div className="App">
             <div style={ { height: '20%'} }>
                 <h1>Scradles</h1>
             </div>
-            <table style={ { width: '100%'} }>
-                <tr>
-                    <td style={ { width: '40%', backgroundColor: 'lightgray'} }>
-                        <GameBoard value="Schere"/>
-                    </td>
-                    <td style={ { width: '20%'} }>
-                        <ScoreBoard score1="win" score2="loss"/>
-                    </td>
-                    <td style={ { width: '40%', backgroundColor: 'lightgray'} }>
-                        <GameBoard value="Stein"/>
-                    </td>
-                </tr>
-            </table>
-      </div>
-  )
+            <Ingame/>
+        </div>
+    )
 }
 
 export default App;
