32dc150230283157b6e156ec86cdd835b6f775b9
Christoph Boni <c.boni@oth-aw.de>
2022-06-01 00:52:39 +0200

    Adds Lobby logik, Clienten werden bei der lobby suche unterteilt

--- sys-src/backend/index.js
+++ sys-src/backend/index.js
@@ -44,14 +44,40 @@ io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
     clientNo++;
 
-    //socket.join(Math.round(clientNo/2));
-    //socket.emit('serverMsg', roomNo);
+    socket.on("join_room", (data, name) => {
 
-    console.log(`User Anzahl: ${clientNo}`);
-    socket.on("join_room", (data) => {
-        socket.join(data);
-        console.log(`User with ID: ${socket.id} joined room: ${data}`)
+        if(io.sockets.adapter.rooms.get(data) == null){
+            console.log("du kannst diesem raum nicht beitreten, er existiert nicht")
+            socket.emit("lobby_null");
+        }
+        else{
+            if(io.sockets.adapter.rooms.get(data).size < 2){
+                socket.join(data);
+                console.log(`lobbysize ${io.sockets.adapter.rooms.get(data).size}`)
+                console.log(`User with ID: ${socket.id} joined room: ${data}`)
+                socket.emit("joined")
+                //schickt allen clients im selben raum die nachricht, alle clients die nach einem gejoint werden beim vorherig gejointen client angezeigt
+                io.to(data).emit("userJoinsLobby", name, io.sockets.adapter.rooms.get(data).size)
+            }
+            else{
+                console.log('Cant join a full lobby')
+                socket.emit("lobby_voll")
+            }
+        }      
     })
+
+    socket.on("create_room", (data, name) =>{
+        if(io.sockets.adapter.rooms.get(data) == null){
+            console.log(`raum ${data} wurde erstellt`)
+            socket.join(data);
+            socket.emit('joined')
+        }
+        else{
+            console.log("er existiert bereits")
+            socket.emit('roomalreadyexists')
+        }
+    })
+
     socket.on("disconnect", () => {
         clientNo--;
         console.log(`User Anzahl: ${clientNo}`);
--- sys-src/frontend/src/components/pages/Room.js
+++ sys-src/frontend/src/components/pages/Room.js
@@ -1,40 +1,64 @@
-import React from 'react';
+import React, { useEffect, useLayoutEffect } from 'react';
 import { useState, setTime } from "react";
 import io from 'socket.io-client';
-import {useNavigate, useParams} from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 
 
-
-    const socket = io('http://localhost:3001')    
-    socket.on('connect', ()=>console.log(socket.id))
-    socket.on('connect_error', ()=>{
-      setTimeout(()=>socket.connect(),5000)
-    })   
-    socket.on('time', (data)=>setTime(data))
-   socket.on('disconnect',()=>setTime('server disconnected'))
-  
+const socket = io('http://localhost:8080')
+socket.on('connect', () => console.log(socket.id))
+socket.on('connect_error', () => {
+  setTimeout(() => socket.connect(), 5000)
+})
+socket.on('time', (data) => setTime(data))
+socket.on('disconnect', () => setTime('server disconnected'))
 
 
-let clientRoom;
+socket.on("lobby_null", () => {
+  //dieser raum exisitiert nicht, erstelle einen raum oder such nach einem existentem raum. 
+})
 
-socket.on('serverMsg', (data) => {
-  console.log(`I should be in room No. ${data}`);
-  clientRoom = data;
+socket.on("lobby_voll", () => {
+  //dieser raum ist voll!
+})
+
+socket.on("roomalreadyexists", () => {
+  //dieser raum existiert bereits
 })
 
 
+let roomsize;
+const players = [];
+
+socket.on("userJoinsLobby", (name, size) => {
+  roomsize = size;
+  players.push(name);
+  // hier werden dem aktuellen client nur die nächst beitretenden clients angezeigt
+  console.log(`User in dem Raum ${players} und die Raumbelegung ${roomsize} von 2`)
+})
+
 function Room() {
+
   let navigate = useNavigate();
+
+  // problem mit react router, springt auf die seite zig mal, funktioniert aber trotzdem. feature?
+  socket.on("joined", () => {
+    navigate("/Lobby");
+  })
 
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
 
   const joinRoom = () => {
-    
+
     if (username !== "" && room !== "") {
-      socket.emit("join_room", room);
-      navigate("/Lobby");
+      socket.emit("join_room", room, username);
+    }
+  }
+  const createRoom = () => {
+
+    if (username !== "" && room !== "") {
+      socket.emit("create_room", room, username);
     }
   }
 
@@ -49,9 +73,10 @@ function Room() {
         setRoom(event.target.value);
       }} />
 
-      <button onClick={ (joinRoom) }>Join a Room
+      <button onClick={(joinRoom)}>Join a Room
       </button>
-
+      <button onClick={(createRoom)}>Create a Room
+      </button>
     </div>
   )
 }
