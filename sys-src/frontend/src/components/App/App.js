382b294e1e4618ddf1fbb5b3009f66c38808d6ed
Aaron Kreuzer <a.kreuzer@oth-aw.de>
2022-06-10 13:13:49 +0200

    layout changes
    Former-commit-id: 92551d1e93784d00f079f5de3a5b5f9802ed8bdd

--- sys-src/frontend/src/components/App/App.js
+++ sys-src/frontend/src/components/App/App.js
@@ -3,18 +3,24 @@ import React from 'react'
 import Room from '../pages/Room';
 import Startpage from '../pages/startpage';
 import Lobby from '../pages/Lobby';
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
 import {Link, BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-  <Router>
-    <Routes>
-      <Route path ="/" element={<Startpage/>}/>
-      <Route path ="/Room" element={<Room/>}/>
-      <Route path="/Lobby" element={<Lobby/>}/>
-      <Route path ="*" element={<Startpage/>}/>
-    </Routes>
-  </Router>
+  <div className='App'>
+    <Header/>
+    <Router>
+      <Routes>
+        <Route path ="/" element={<Startpage/>}/>
+        <Route path ="/Room" element={<Room/>}/>
+        <Route path="/Lobby" element={<Lobby/>}/>
+        <Route path ="*" element={<Startpage/>}/>
+      </Routes>
+    </Router>
+    <Footer/>
+  </div>
   
   );
 }
