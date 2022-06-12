25b3b3dddacf0ed1fbf6a9cda91b2052d1609228
Leonard Wöllmer <l.woellmer@oth-aw.de>
2022-06-13 00:08:03 +0200

    ranked ladder navigation added
    Former-commit-id: 71eda32a7015a28d4697d25461ada171d558fd97

--- sys-src/frontend/src/components/Header/Header.js
+++ sys-src/frontend/src/components/Header/Header.js
@@ -1,14 +1,18 @@
 import './Header.css';
+import {useNavigate} from 'react-router-dom';
 
 function Header(props) {
 
+    let navigate = useNavigate();
+
     return(
         <div className='Header'>
-            <h1 className='header-title'>
+            <h1 className='header-title'
+                onClick={() => {navigate("/");}}>
                 Scradles
             </h1>
             <nav>
-                <a>Placeholder 1</a>
+                <a onClick={() => {navigate("/RankedLadder");}}>Rangliste</a>
                 <a>Placeholder 2</a>
                 <a>Placeholder 3</a>
                 <a>Placeholder 4</a>
