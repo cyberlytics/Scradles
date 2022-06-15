3056d79a691208d77e71f2a0027e83c3f211d0ce
Leonard Wöllmer <l.woellmer@oth-aw.de>
2022-06-15 18:44:06 +0200

    Add pagination to Leaderboard.
    Former-commit-id: 2917b94fe8841be1f7d6bcc2b1f6b39b4d79f98b

--- sys-src/backend/routes/leaderboardRoute.js
+++ sys-src/backend/routes/leaderboardRoute.js
@@ -6,6 +6,9 @@ let leaderboardController = require('../controllers/leaderboardController');
 //Route to get all entries
 router.get('/all', leaderboardController.leaderboard_all);
 
+//Route to get entries with pagination
+router.get('/bulk/:page', leaderboardController.leaderboard_bulk);
+
 //Route to add new entry
 router.get('/add',leaderboardController.leaderboard_add);
 
