fbbb18329306da7e19b21a92fc998aad500f153d
Leonard Woellmer <l.woellmer@oth-aw.de>
2022-06-15 18:44:06 +0200

    Add pagination to Leaderboard.

--- sys-src/frontend/src/components/Leaderboard/Leaderboard.js
+++ sys-src/frontend/src/components/Leaderboard/Leaderboard.js
@@ -5,23 +5,41 @@ import './Leaderboard.css';
 function Leaderboard(props) {
 
     const [data, setData] = useState([]);
+    const [page, setPage] = useState(0);
 
     useEffect(() => {
-        fetch('http://localhost:8080/leaderboard/all')
+        fetch('http://localhost:8080/leaderboard/bulk/' + page)
         .then(response => response.json())
         .then(data => setData(data))
         .catch(error => console.error(error));
-    }, []);
+    }, [page]);
+
+    const PageUp = () =>{
+        setPage(page + 1);
+    }
+
+    const PageDown = () => {
+        if(page != 0){
+            setPage(page - 1);
+        }
+    }
 
     return(
         <div className='Leaderboard'>
-            {data.map((args, i) => {
-                return(
-                    <div key={args._id} className='LeaderboardItem'>
-                        <LeaderboardElement number={i + 1} score={args.points} player={args.name} />
-                    </div>
-                );
-            })}
+            <div className='LeaderboardGrid'>
+                {data.map((args, i) => {
+                    return(
+                        <div key={args._id} className='LeaderboardItem'>
+                            <LeaderboardElement number={(10 * page) + i + 1} score={args.points} player={args.name} />
+                        </div>
+                    );
+                })}
+            </div>
+            <div className='Pagination'>
+                <a onClick={PageDown} href="#">&laquo;</a>
+                <a className='Page'>{page}</a>
+                <a onClick={PageUp} href="#">&raquo;</a>
+            </div>
         </div>
     );    
 }
