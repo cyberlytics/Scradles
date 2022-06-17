ab718122215a6a9e760b9be4c1ff325609a2d146
d7df <a.rall@oth-aw.de>
2022-06-17 15:08:47 +0200

    Images können nun angezeigt werden statt Text

--- sys-src/frontend/src/components/GameBoard/GameBoard.js
+++ sys-src/frontend/src/components/GameBoard/GameBoard.js
@@ -1,18 +1,26 @@
 import PropTypes from 'prop-types';
+import react from 'react';
+
+import './GameBoard.css';
 import Selection from '../Selection/Selection';
+
+import scissors from '../images/scissors.svg'
+import paper from '../images/paper.svg'
+import rock from '../images/rock.svg'
 
 function GameBoard(props) {
 
-    return(
+    return (
         <div>
-            <p>{props.value}</p>
+            <img className={"gameboard-image"}
+                src={
+                    props === "rock" ? rock : props === "paper" ? paper : scissors
+                }
+                alt="rock scissors paper">
+            </img>
             <Selection></Selection>
         </div>
-    );    
-}
-
-GameBoard.propTypes = {
-    value: PropTypes.string
+    );
 }
 
 export default GameBoard;
