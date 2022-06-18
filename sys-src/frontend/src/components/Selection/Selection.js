cc4048ceca1c9a7bd98597de0ccd7b26e24684fd
Aaron Kreuzer <a.kreuzer@oth-aw.de>
2022-06-18 17:47:07 +0200

    show players in lobby + selection
    Former-commit-id: 1b18602b9edd9bd11f7466603b9e7a4503a2be8d

---
+++ sys-src/frontend/src/components/Selection/Selection.js
@@ -0,0 +1,14 @@
+import './selection.css';
+
+function Selection(props) {
+
+    return(
+        <div className='selection'>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+        </div>
+    );    
+}
+
+export default Selection;
--- sys-src/frontend/src/components/pages/Lobby.js
+++ sys-src/frontend/src/components/pages/Lobby.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useContext ,useEffect, useState} from 'react';
 import Ingame from '../Ingame/Ingame'
 import './pages.css';
 import { useLocation } from 'react-router-dom';
+import {SocketContext} from '../../context/socket';
 
 function Lobby(){
     // hier lesen wir aus in welchem mode wir uns befinden
@@ -9,12 +10,33 @@ function Lobby(){
     // dann verwenden wir eine lobby für alles können aber ja nach mode anpassungen durchführen
     // navigate("/Lobby", {state: {mode: 'createdRoom'}});
     const {state} = useLocation();
-    const { mode } = state;
-    console.log(mode)
+    const { mode, gameobject, playerNumber} = state;
+
+    const socket = useContext(SocketContext);
+    const [gameobjectState, setGameobject] = useState(gameobject);
+
+    let roomsize;
+    let userjoined;
+
+    console.log(gameobject)
+    useEffect(() => {
+        socket.on("userJoinsLobby", (gameobject, size) =>{
+          roomsize = size;
+          // hier wird dem aktuellen client das gameobject zur verfügung gestellt
+          setGameobject(gameobject);
+        })
+    
+        return () => {
+          // hier sollten auch noch die genau callbacks angegeben werden (socket.off("lobby_null", this.onLobbyFull))
+          // dafür irgendwann die funktionen auslagern
+          // aktuell werden so alle callbacks für dieses event entfernt
+          socket.off("userJoinsLobby");
+        }
+      }, [socket])
 
     return(
         <div className='container'>
-            <Ingame/>
+            <Ingame gameobject={gameobjectState} playerNumber={playerNumber} />
         </div>
     )
 }
