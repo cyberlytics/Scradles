5a8fb81dddda5a56861111319fd1e4f8a88079f3
Adrian Rall <a.rall@oth-aw.de>
2022-06-24 23:15:53 +0200

    CSS verbessert Image nach Buttonklick nicht hinbekommen, einzubinden
    Former-commit-id: 6b114ec5adf47889123bbf6c3ac00945ee29ac9c

--- sys-src/frontend/src/components/Selection/Selection.js
+++ sys-src/frontend/src/components/Selection/Selection.js
@@ -3,7 +3,6 @@ import './selection.css';
 import { ReactComponent as Schere } from '../images/scissors.svg';
 import { ReactComponent as Stein } from '../images/rock.svg';
 import { ReactComponent as Papier } from '../images/paper.svg';
-//import Papier from '../images/hand-scissors.png';
 
 // function Selection(props) {
 
@@ -21,12 +20,9 @@ import { ReactComponent as Papier } from '../images/paper.svg';
 function Selection(props) {
     return (
         <div className='selection'>
-            <button className="round-btn" onClick={() => props.onSelectionChange(props === "Papier" ? <img src={Papier} alt="Schere"></img> : <Papier />)}><Papier /></button>
-            <button className="round-btn" onClick={() => props.onSelectionChange(props === "Schere" ? <img src={Schere} alt="Schere"></img> : "Schere")}><Schere /></button>
-            <button className="round-btn" onClick={() => props.onSelectionChange(props === "Stein" ? <img src={Stein} alt="Stein"></img> : "Stein")}><Stein /></button>
-            {/* <button onClick={() => props.onSelectionChange("Schere")}>Schere</button>
-            <button onClick={() => props.onSelectionChange("Stein")}>Stein</button>
-            <button onClick={() => props.onSelectionChange("Papier")}>Papier</button> */}
+            <button className="round-btn" onClick={() => props.onSelectionChange(props === "Papier" ? <Papier /> : "Papier")}><Papier /></button>
+            <button className="round-btn" onClick={() => props.onSelectionChange(props === "Schere" ? <Schere /> : "Scherenhand")}><Schere /></button>
+            <button className="round-btn" onClick={() => props.onSelectionChange(props === "Stein" ? <Stein /> : "Stein")}><Stein /></button>
         </div>
     );
 }
