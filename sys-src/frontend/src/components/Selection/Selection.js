bd09624353b2e7ecf65dc14dba132607e4c6d3f4
Adrian Rall <a.rall@oth-aw.de>
2022-06-24 19:04:26 +0200

    Buttons als Icons
    Former-commit-id: bbf26ebbf33896c20d5d678d3b79dcdc422cf0fb

--- sys-src/frontend/src/components/Selection/Selection.js
+++ sys-src/frontend/src/components/Selection/Selection.js
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types';
 import './selection.css';
-import Schere from '../images/scissors.svg';
-import Stein from '../images/rock.svg';
-import Papier from '../images/paper.svg';
+import { ReactComponent as Schere } from '../images/scissors.svg';
+import { ReactComponent as Stein } from '../images/rock.svg';
+import { ReactComponent as Papier } from '../images/paper.svg';
 //import Papier from '../images/hand-scissors.png';
 
 // function Selection(props) {
@@ -19,12 +19,11 @@ import Papier from '../images/paper.svg';
 // }
 
 function Selection(props) {
-
     return (
         <div className='selection'>
-            <button onClick={() => props.onSelectionChange(props === "Papier" ? <img src={Papier} alt="Papier"></img> : "Papier")}>Papier</button>
-            <button onClick={() => props.onSelectionChange(props === "Schere" ? <img src={Schere} alt="Schere"></img> : "Schere")}>Schere</button>
-            <button onClick={() => props.onSelectionChange(props === "Stein" ? <img src={Stein} alt="Stein"></img> : "Stein")}>Stein</button>
+            <button className="round-btn" onClick={() => props.onSelectionChange(props === "Papier" ? <img src={Papier} alt="Schere"></img> : <Papier />)}><Papier /></button>
+            <button className="round-btn" onClick={() => props.onSelectionChange(props === "Schere" ? <img src={Schere} alt="Schere"></img> : "Schere")}><Schere /></button>
+            <button className="round-btn" onClick={() => props.onSelectionChange(props === "Stein" ? <img src={Stein} alt="Stein"></img> : "Stein")}><Stein /></button>
             {/* <button onClick={() => props.onSelectionChange("Schere")}>Schere</button>
             <button onClick={() => props.onSelectionChange("Stein")}>Stein</button>
             <button onClick={() => props.onSelectionChange("Papier")}>Papier</button> */}
