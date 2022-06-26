dd0a2238549d35b4b2b9377c27b8455cfe6f7477
Leonard Woellmer <l.woellmer@oth-aw.de>
2022-06-26 16:50:07 +0200

    Add tests to backend (gamelobbyfunctions).

--- sys-src/backend/game/gamelobbyfunctions.js
+++ sys-src/backend/game/gamelobbyfunctions.js
@@ -47,17 +47,17 @@ exports.leaveGame = function leaveGame(socketID) {
                 if (element2.socket.includes(socketID)) {
                     const index = element.players.indexOf(element2);
                     if (index == 0) {
-                        console.log("creator verlässt raum")
+                        console.log("creator verlässt raum");
                         copyelement = structuredClone(element);
-                        games.splice(games.indexOf(element), 1)
+                        games.splice(games.indexOf(element), 1);
                         // game wird aus dem komplettem array gelöscht wenn creator den raum verlässt
                         return copyelement;
                         }
                     else {
                         // ansonsten wird nur der player aus dem array players gelöscht
-                        copyelement2 = structuredClone(element)
-                        console.log("spieler 2 verlässt raum")
-                        element.players.splice(index, 1)
+                        copyelement2 = structuredClone(element);
+                        console.log("spieler 2 verlässt raum");
+                        element.players.splice(index, 1);
                         return copyelement2;
                     }
                 }
