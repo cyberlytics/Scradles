bfb4333db5bc470af6855c9f85d5a5c2449fb5e4
Kreuzer, Aaron <a.kreuzer@oth-aw.de>
2022-07-03 18:52:02 +0200

    styling and naming changes + sign fixes

--- sys-src/frontend/src/components/GameBoard/GameBoard.js
+++ sys-src/frontend/src/components/GameBoard/GameBoard.js
@@ -7,22 +7,21 @@ import { ReactComponent as Papier } from '../images/paper.svg';
 import Summary from '../Summary/Summary';
 
 function GameBoard(props) {
-    let selection
-    let image
+    let selection = <></>
+    let image = <></>
+    
     if (props.main === true && props.gameState !== 'roundWinner' && props.gameState !== null && props.gameState !== 'gameWinner') {
         selection = <Selection onSelectionChange={props.onSelectionChange} />
-        if (props.value === "Schere") {
-            image = <Schere />
-        }
-        else if (props.value === 'Stein') {
-            image = <Stein />
-        }
-        else if (props.value === 'Papier') {
-            image = <Papier />
-        }
-    } else {
-        selection = <></>
-        image = <></>
+    }
+
+    if (props.value === "Schere") {
+        image = <Schere />
+    }
+    else if (props.value === 'Stein') {
+        image = <Stein />
+    }
+    else if (props.value === 'Papier') {
+        image = <Papier />
     }
 
     return (
